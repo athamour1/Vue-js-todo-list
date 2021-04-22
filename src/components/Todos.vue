b7eb32a8e8e81edc50fee4999f6ca998339a7d23
athamour1 <thanosmour.tk@gmail.com>
2021-04-22 13:18:38 +0300

    line-through when completed

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <div v-for="todo in todos"  v-bind:key="todo">
-            <h3>{{todo.title}}</h3>
+        <div v-for="todo in todos"  v-bind:key="todo.id">
+            <TodoItem v-bind:todo="todo"/>
         </div>
     </div>
 </template>
 <script>
+import TodoItem from './TodoItem.vue'
+
 export default {
     name: "Todos",
+    components: {
+        TodoItem
+    },
     props: ["todos"]
 }
 </script>
